c83340c3962a95e567bdf10fef0bc552bddffe01
Michael Lee <lee.5151@osu.edu>
2018-09-14 21:18:38 -0400

    add solutions

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,33 @@
-#write your code here
+def echo(string)
+  string
+end
+
+def shout(string)
+  string.upcase
+end
+
+def repeat(string, num = 2)
+  strings = []
+  num.times do
+    strings << string
+  end
+  strings.join(" ")
+end
+
+def start_of_word(string, num)
+  string.slice(0, num)
+end
+
+def first_word(string)
+  words = string.split
+  words[0]
+end
+
+def titleize(string)
+  strings = string.split
+  little_words = ["and", "over", "the"]
+  strings.each do |string|
+    string.capitalize! if little_words.index(string) == nil || strings[0] == string
+  end
+  strings.join(" ")
+end
--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,12 @@
 class Book
-# write your code here
+  attr_reader :title
+
+  def title=(string)
+    strings = string.split
+    little_words = ["and", "over", "the", "in", "of", "a", "an"]
+    strings.each do |string|
+    string.capitalize! if little_words.index(string) == nil || strings[0] == string
+    end
+    @title = strings.join(" ")
+  end
 end
